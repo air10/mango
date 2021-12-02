c6020a438e49c110559efb1c4f9df4a9307ca929
hanlei <14816856@qq.com>
2021-12-02 22:03:45 +0800

    第11章 登录流程实现

---
+++ mango-admin/src/main/java/com/louis/mango/admin/controller/SysUserController.java
@@ -0,0 +1,41 @@
+package com.louis.mango.admin.controller;
+
+import java.io.File;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.louis.mango.admin.service.SysUserService;
+import com.louis.mango.common.utils.FileUtils;
+import com.louis.mango.core.http.HttpResult;
+import com.louis.mango.core.page.PageRequest;
+
+@RestController
+@RequestMapping("user")
+public class SysUserController {
+
+    @Autowired
+    private SysUserService sysUserService;
+    
+    @GetMapping(value="/findAll")
+    public Object findAll() {
+        return sysUserService.findAll();
+    }
+    
+	@PostMapping(value="/findPage")
+	public HttpResult findPage(@RequestBody PageRequest pageRequest) {
+		return HttpResult.ok(sysUserService.findPage(pageRequest));
+	}
+	
+	@PostMapping(value="/exportExcelUser")
+	public void exportExcelUser(@RequestBody PageRequest pageRequest, HttpServletResponse res) {
+		File file = sysUserService.createUserExcelFile(pageRequest);
+		FileUtils.downloadFile(res, file, file.getName());
+	}
+}
