3e2570f68342bd2687017f720d32da6bfb07d353
hanlei <14816856@qq.com>
2021-12-04 00:41:55 +0800

    13章系统服务监控完成 （admin 和backup项目 添加了spring-boot-admin-starter-client，新建了monitor工程，作为server）

---
+++ mango-monitor/src/main/java/com/louis/mango/monitor/MangoMonitorApplication.java
@@ -0,0 +1,15 @@
+package com.louis.mango.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+@EnableAdminServer
+@SpringBootApplication
+public class MangoMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoMonitorApplication.class, args);
+	}
+
+}
