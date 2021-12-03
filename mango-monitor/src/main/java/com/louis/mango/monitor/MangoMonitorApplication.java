d0824090305bf00156cc3ccd49ec86477f0c7d0c
hanlei <14816856@qq.com>
2021-12-04 01:45:41 +0800

    第14章 注册中心 完成 backup  monitor admin 都连接到了 consul ，三个项目内到cloud版本需要用   <version>Greenwich.RELEASE</version>，才能正常连接到consul。

--- mango-monitor/src/main/java/com/louis/mango/monitor/MangoMonitorApplication.java
+++ mango-monitor/src/main/java/com/louis/mango/monitor/MangoMonitorApplication.java
@@ -2,10 +2,12 @@ package com.louis.mango.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 @EnableAdminServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MangoMonitorApplication {
 
 	public static void main(String[] args) {
